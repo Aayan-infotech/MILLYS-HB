d51c8cb4deea74436fe1eb80fc2864f50846f737
Vikas Mohan <vikas.mohan@aayaninfotech.com>
2024-07-29 10:31:12 +0530

    ui

---
+++ flutter_project/flutter_project/flutter_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_projectApp.swift
+//  flutter_project
+//
+//  Created by mac on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct flutter_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
